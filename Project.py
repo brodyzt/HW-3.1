f94724a68b122b25659e21c92e21cf1b0db803e9
brodyzt <brodyzt@gmail.com>
2015-06-29 11:38:56 -0400

    update

--- Project.py
+++ Project.py
@@ -1,1 +1,10 @@
-__author__ = 'brodyzt'
+class Bus:
+    def __init__(self, numSeatsTotal, numSeatsFilled, depart, arrive):
+        self.numSeatsTotal = numSeatsTotal
+        self.numseatsFilled = numSeatsFilled
+        self.depart = depart
+        self.arrive = arrive
+
+    def isFilled(self):
+        return self.numseatsFilled == self.numSeatsTotal
+
