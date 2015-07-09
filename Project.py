b5fb50a3f31236fd2d63fe5443d1f0e9bdd094e0
brodyzt <brodyzt@gmail.com>
2015-07-09 12:31:14 -0400

    update

--- Project.py
+++ Project.py
@@ -1,29 +1,142 @@
 import math
 
-class Bus:
-    def __init__(self, numSeatsTotal, numSeatsFilled, depart, arrive):
-        self.numSeatsTotal = numSeatsTotal
-        self.numseatsFilled = numSeatsFilled
-        self.depart = depart
-        self.arrive = arrive
+class EmptySeatFound(Exception): pass
 
-    def isFilled(self):
-        return self.numseatsFilled == self.numSeatsTotal
+class Person:
+    def __init__(self, id=None, name=None, age=None, departure_city=None, arrival_city=None, personality=None):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.departure_city = departure_city
+        self.arrival_city = arrival_city
+        self.personality = personality
+
+    def get_data(self):
+        list = []
+        list.append(self.id)
+        list.append(self.name)
+        list.append(self.age)
+        list.append(self.departure_city)
+        list.append(self.arrival_city)
+        list.append(self.personality)
+        return list
+class Seat:
+    def __init__(self):
+        self.is_filled = False
+        self.sitting_person = None
+
+    def seat_person(self, person):
+        self.sitting_person = person
+class Bus:
+    def __init__(self, seat_rows, seat_columns, departure_city, arrival_city):
+        self.departure_city = departure_city
+        self.arrival_city = arrival_city
+        temp_grid = []
+        for y in range(seat_rows):
+            temp_row = []
+            for x in range(seat_columns):
+                temp_row.append(Seat())
+            temp_grid.append(temp_row)
+        self.seats = temp_grid
+
+    def is_filled(self):
+        for row in self.seats:
+            for seat in row:
+                if not seat.is_filled:
+                    return False
+        return True
+
+    def add_person(self, person):
+        for row in self.seats:
+            for seat in row:
+                if not seat.is_filled:
+                    seat.seat_person(person)
+                    return None
+
+    def string_format(self):
+        pass
 class City:
     def __init__(self, name, latitude, longitude):
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
 
-def list_of_cities():
-    list = []
-    file = open('Cities','r')
-    contents = file.readlines()
-    for line in contents:
-        currentCity = line.split(',')
-        list.append(City(currentCity[0],float(currentCity[1]),float(currentCity[2])))
-    file.close()
-    return list
+class ProgramStartup():
+
+    @staticmethod
+    def get_cities():
+        list = []
+        file = open('Cities','r')
+        contents = file.readlines()
+        contents.pop(0)
+        for line in contents:
+            current_city = line.split(',')
+            list.append(City(current_city[0],float(current_city[1]),float(current_city[2])))
+        file.close()
+        return list
+
+    @staticmethod
+    def get_people():
+        list = []
+        file = open('People','r')
+        contents = file.readlines()
+        contents.pop(0)
+        for line in contents:
+            temp_person = Person()
+            attributes = line.split(',')
+            temp_person.id = int(attributes[0])
+            temp_person.name = attributes[1]
+            temp_person.age = int(attributes[2])
+            temp_person.departure_city = attributes[3]
+            temp_person.arrival_city = attributes[4]
+            temp_person.personality = int(attributes[5])
+            list.append(temp_person)
+        return list
+
+    def get_seats:
+
+class Sort():
+
+    @staticmethod
+    def bubble(list):
+        is_sorted = False
+        while not is_sorted:
+            is_sorted = True
+            for x in range(len(list) - 1):
+                if(list[x] > list[x+1]):
+                    temp = list[x]
+                    list[x] = list[x+1]
+                    list[x+1] = temp
+                    is_sorted = False
+        return list
+    @staticmethod
+    def selection(list):
+        for x in range(0, len(list) - 1):
+            temp_lowest = list[x]
+            temp_lowest_location = x
+            for y in range(x + 1, len(list)):
+                if list[y] < temp_lowest:
+                    temp_lowest = list[y]
+                    temp_lowest_location = y
+            temp = list[x]
+            list[x] = list[temp_lowest_location]
+            list[temp_lowest_location] = temp
+        return list
+    @staticmethod
+    def insertion(list):
+        temp_list = []
+        list_length = len(list)
+        for x in range(list_length):
+            min = list[0]
+            min_loc = 0
+            for y in range(1, len(list)):
+                if list[y] < list[min_loc]:
+                    min = list[y]
+                    min_loc = y
+            temp_list.append(list[min_loc])
+            list.pop(min_loc)
+        return temp_list
+
 def dis_bet_cities(city1, city2):
     lat1 = math.radians(city1.latitude)
     lat2 = math.radians(city2.latitude)
@@ -66,55 +179,64 @@ def delete_city(name):
 def print_cities(list_of_cities):
     for x in range(len(list_of_cities)):
             print(str(x+1) + '.' + list_of_cities[x].name)
+def city_with_name(city_name, list_input):
+    for city in list_input:
+        if(city_name == city.name):
+            return city
 
-cities = list_of_cities()
 
-running = True
 
-while(running):
-    print('Welcome to the bus network. What would you like to do?')
-    print('\n1.Calculate distance\n2.List Current City Options\n3.Add City\n4.Delete City\n')
-    choice = input('Choice: ')
-    clear_screen()
+def bus_network():
+    cities = ProgramStartup.get_cities()
+    people = ProgramStartup.get_people()
 
-    if(choice == '1'):
-        print('The following list are cities with bus stops. Please select a departure city and an arrival city.\n')
-        print_cities(cities)
-        city1 = cities[int(input('\nDeparture city: ')) - 1]
-        city2 = cities[int(input('Arrival city: ')) - 1]
-        distance = dis_bet_cities(city1,city2)
+    print(people[0].get_data())
+
+    running = True
+    while(running):
+        print('Welcome to the bus network. What would you like to do?')
+        print('\n1.Calculate distance\n2.List Current City Options\n3.Add City\n4.Delete City\n')
+        choice = input('Choice: ')
         clear_screen()
-        print('\nThe distance between {} and {} is {} miles'.format(city1.name,city2.name,dis_bet_cities(city1,city2)))
-        print_lines(5)
-        running = will_continue()
 
-    if(choice == '2'):
-        print_cities(cities)
-        input('\nHit enter when done')
-        clear_screen()
-        running = will_continue()
+        if choice == '1':
+            print('The following list are cities with bus stops. Please select a departure city and an arrival city.\n')
+            print_cities(cities)
+            city1 = cities[int(input('\nDeparture city: ')) - 1]
+            city2 = cities[int(input('Arrival city: ')) - 1]
+            distance = dis_bet_cities(city1,city2)
+            clear_screen()
+            print('\nThe distance between {} and {} is {} miles'.format(city1.name,city2.name,dis_bet_cities(city1,city2)))
+            print_lines(5)
+            running = will_continue()
 
-    if(choice == '3'):
-        print('Please enter the details of the city you would like to add\nIf the coordinates are in the Northern or Western hemispheres, don\'t forget the negative sign\n')
-        name = input('Name: ')
-        latitude = float(input('Latitude: '))
-        longitude = float(input('Longitude: '))
-        add_city(City(name,latitude,longitude))
-        clear_screen()
-        running = will_continue()
+        if choice == '2':
+            print_cities(cities)
+            input('\nHit enter when done')
+            clear_screen()
+            running = will_continue()
 
-    if(choice == '4'):
-        print('Please select which city you would like to delete\n')
-        for x in range(len(cities)):
-            print(str(x+1) + '.' + cities[x].name)
-        choice = input('\nChoice: ')
-        if(choice.isnumeric() == True and 0 < int(choice) <= len(cities)):
-            city_name = cities[int(choice) - 1].name
-            cities = delete_city(city_name)
-        clear_screen()
-        running = will_continue()
+        if choice == '3':
+            print('Please enter the details of the city you would like to add\nIf the coordinates are in the Northern or Western hemispheres, don\'t forget the negative sign\n')
+            name = input('Name: ')
+            latitude = float(input('Latitude: '))
+            longitude = float(input('Longitude: '))
+            add_city(City(name,latitude,longitude))
+            clear_screen()
+            running = will_continue()
 
+        if choice == '4':
+            print('Please select which city you would like to delete\n')
+            for x in range(len(cities)):
+                print(str(x+1) + '.' + cities[x].name)
+            choice = input('\nChoice: ')
+            if choice.isnumeric() == True and 0 < int(choice) <= len(cities):
+                city_name = cities[int(choice) - 1].name
+                cities = delete_city(city_name)
+            clear_screen()
+            running = will_continue()
 
+bus_network()  # runs the main program
 
 
 
